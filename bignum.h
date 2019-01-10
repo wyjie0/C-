e72e46b692cefef39c081e61c3e431a26fa115f6
wyjie0 <670865824@qq.com>
2019-01-10 17:47:51 +0800

    Update bignum.h

--- bignum.h
+++ bignum.h
@@ -2,21 +2,23 @@
 #include<string.h>
 #include<stdlib.h>
 
+#define MAX 1000
+
 typedef struct biging
 {
-	char* num;//指向长整数数组（序号0中保存着最高位） 
+	char *num;//指向长整数数组（序号0中保存着最高位） 
 	char sign;//符号（1表示正数，-1表示负数） 
 	int digit;//保存该数的位数 
-} BigInt, *pBigInt;
+} BigInt;
 
 //加法运算 
-pBigInt BigIntAdd(pBigInt num1, pBigInt num2);
+BigInt* BigIntAdd(BigInt num1, BigInt num2);
 
 //减法运算
-void BigIntSub(pBigInt num1, pBigInt num2, pBigInt result);
-
-//乘法运算
-void BigIntMul(pBigInt num1, pBigInt num2, pBigInt result); 
-
-//除法运算
-void BigIntDiv(pBigInt num1, pBigInt num2, pBigInt result);  
+//void BigIntSub(pBigInt num1, pBigInt num2, pBigInt result);
+//
+////乘法运算
+//void BigIntMul(pBigInt num1, pBigInt num2, pBigInt result); 
+//
+////除法运算
+//void BigIntDiv(pBigInt num1, pBigInt num2, pBigInt result);  
